969b5948a7d06bce72ffb9e5f005646677c471c4
Sheng Hao Ong <shenghaoong@gmail.com>
2026-03-25 21:15:52 +0800

    Add orderbook slice protocol and added more methods to iorderbook

--- src/orderbook/orderbook_protocol.py
+++ src/orderbook/orderbook_protocol.py
@@ -2,6 +2,7 @@ from typing import Protocol
 from abc import abstractmethod
 from uuid import uuid4, UUID
 from enum import Enum
+from collections.abc import Collection, Sequence
 
 from src.order.order_protocol import IOrder
 from src.util import OrderTypeEnum
@@ -14,6 +15,10 @@ class IOrderStatus(Enum):
 
 
 class IOrderBookOrder(Protocol):
+    """
+    Orderbook Order that is attributed to an owner
+    """
+
     order_type: OrderTypeEnum
     order_qty: int # set to int for now
     order_owner: UUID
@@ -22,12 +27,31 @@ class IOrderBookOrder(Protocol):
     def fill_order(self, IOrder) -> 'IOrderBookOrder':
         raise NotImplementedError
 
+class IOrderBookSlice(Protocol):
+    """
+    Aggregation of IOrderBookOrder at a specific price
+    """
+    orders: Sequence[IOrderBookOrder]
+    # Orders in slice must have some ordering to determine which order to fill first
+
+    @abstractmethod
+    def fill_order(self, IOrder) -> 'IOrderBookSlice':
+        raise NotImplementedError
+
 
 class IOrderBook(Protocol):
-    
+    open_orders: Collection[IOrderBookSlice]
     
     @abstractmethod
     def process_order(self, IOrder) -> IOrderStatus:
         raise NotImplementedError
 
+    @abstractmethod
+    def get_bid(self) -> IOrderBookSlice:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_ask(self) -> IOrderBookSlice:
+        raise NotImplementedError
+    
 
