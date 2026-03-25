0efa6f2b640b98e3bb76e114034b6eee1ccd578c
Sheng Hao Ong <shenghaoong@gmail.com>
2026-03-25 20:54:22 +0800

    Rename interface files

--- src/orderbook/orderbook_protocol.py
+++ src/orderbook/orderbook_protocol.py
@@ -1,14 +1,13 @@
 from typing import Protocol
 from abc import abstractmethod
 
-from order.iorder import IOrder
+from src.order.order_protocol import IOrder
 
 class IOrderBook(Protocol):
-    def __init__(self):
-        raise NotImplementedError
+    
     
     @abstractmethod
     def process_order(self, IOrder):
         raise NotImplementedError
-    
+
 
